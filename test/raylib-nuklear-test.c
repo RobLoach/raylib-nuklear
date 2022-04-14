0b0521cb53f038e4a0eb2c6401d7abb0de7ac4de
Rob Loach <robloach@gmail.com>
2022-04-14 19:54:54 -0400

    Add more clear image testing

--- test/raylib-nuklear-test.c
+++ test/raylib-nuklear-test.c
@@ -29,18 +29,17 @@ int main(int argc, char *argv[]) {
     assert(image.handle.ptr);
     Texture texture = TextureFromNuklear(image);
     assert(texture.width > 0);
-    UnloadNuklearImage(image);
 
     // UpdateNuklear()
     UpdateNuklear(ctx);
 
     // Nuklear GUI Code
     // https://github.com/Immediate-Mode-UI/Nuklear/wiki/Window
-    if (nk_begin(ctx, "Nuklear", nk_rect(100, 100, 220, 220),
+    if (nk_begin(ctx, "Nuklear", nk_rect(50, 50, 400, 400),
                     NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE)) {
-        if (nk_button_label(ctx, "Button")) {
-            /* event handling */
-        }
+        nk_button_label(ctx, "Button");
+        nk_layout_row_static(ctx, 256, 256, 1);
+        nk_image(ctx, image);
     }
     nk_end(ctx);
 
@@ -52,6 +51,10 @@ int main(int argc, char *argv[]) {
         DrawNuklear(ctx);
 
     EndDrawing();
+    WaitTime(500);
+
+    // UnloadNuklearImage()
+    UnloadNuklearImage(image);
 
     // UnloadNuklear()
     UnloadNuklear(ctx);
