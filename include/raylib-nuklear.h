b13f39e320deeb8acf0e23c2ccd9565723970273
Rob Loach <robloach@gmail.com>
2024-01-22 02:30:52 -0500

    v5.0.0

--- include/raylib-nuklear.h
+++ include/raylib-nuklear.h
@@ -1,12 +1,12 @@
 /**********************************************************************************************
 *
-*   raylib-nuklear v4.5.1 - Nuklear GUI for Raylib.
+*   raylib-nuklear v5.0.0 - Nuklear GUI for Raylib.
 *
 *   FEATURES:
 *       - Use the Nuklear immediate-mode graphical user interface in raylib.
 *
 *   DEPENDENCIES:
-*       - raylib 4.2+ https://www.raylib.com/
+*       - raylib 4.5+ https://www.raylib.com/
 *       - Nuklear https://github.com/Immediate-Mode-UI/Nuklear
 *
 *   LICENSE: zlib/libpng
