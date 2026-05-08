7dfb3eec5b9006d2474c9cc9c599236adf26ef56
Rob Loach <robloach@gmail.com>
2026-05-08 09:50:32 -0400

    Add tests for NkKeyToKeyboardKey/KeyboardKeyToNkKey

--- test/raylib-nuklear-test.c
+++ test/raylib-nuklear-test.c
@@ -109,6 +109,51 @@ int main(int argc, char *argv[]) {
         UnloadNuklear(ctx);
     }
 
+    // NkKeyToKeyboardKey()
+    {
+        AssertEqual(NkKeyToKeyboardKey(0), KEY_NULL);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_ENTER), KEY_ENTER);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_TAB), KEY_TAB);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_SHIFT), KEY_LEFT_SHIFT);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_BACKSPACE), KEY_BACKSPACE);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_TEXT_RESET_MODE), KEY_ESCAPE);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_DEL), KEY_DELETE);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_UP), KEY_UP);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_DOWN), KEY_DOWN);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_LEFT), KEY_LEFT);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_RIGHT), KEY_RIGHT);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_F1), KEY_F1);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_F6), KEY_F6);
+        AssertEqual(NkKeyToKeyboardKey((nk_rune)NK_KEY_F12), KEY_F12);
+        AssertEqual(NkKeyToKeyboardKey('A'), KEY_A);
+        AssertEqual(NkKeyToKeyboardKey('a'), KEY_A);
+        AssertEqual(NkKeyToKeyboardKey('Z'), KEY_Z);
+        AssertEqual(NkKeyToKeyboardKey('!'), KEY_ONE);
+        AssertEqual(NkKeyToKeyboardKey('@'), KEY_TWO);
+        AssertEqual(NkKeyToKeyboardKey('+'), KEY_EQUAL);
+        AssertEqual(NkKeyToKeyboardKey('_'), KEY_MINUS);
+    }
+
+    // KeyboardKeyToNkKey()
+    {
+        AssertEqual(KeyboardKeyToNkKey(KEY_ENTER), (nk_rune)NK_KEY_ENTER);
+        AssertEqual(KeyboardKeyToNkKey(KEY_TAB), (nk_rune)NK_KEY_TAB);
+        AssertEqual(KeyboardKeyToNkKey(KEY_LEFT_SHIFT), (nk_rune)NK_KEY_SHIFT);
+        AssertEqual(KeyboardKeyToNkKey(KEY_RIGHT_SHIFT), (nk_rune)NK_KEY_SHIFT);
+        AssertEqual(KeyboardKeyToNkKey(KEY_BACKSPACE), (nk_rune)NK_KEY_BACKSPACE);
+        AssertEqual(KeyboardKeyToNkKey(KEY_ESCAPE), (nk_rune)NK_KEY_TEXT_RESET_MODE);
+        AssertEqual(KeyboardKeyToNkKey(KEY_DELETE), (nk_rune)NK_KEY_DEL);
+        AssertEqual(KeyboardKeyToNkKey(KEY_UP), (nk_rune)NK_KEY_UP);
+        AssertEqual(KeyboardKeyToNkKey(KEY_DOWN), (nk_rune)NK_KEY_DOWN);
+        AssertEqual(KeyboardKeyToNkKey(KEY_LEFT), (nk_rune)NK_KEY_LEFT);
+        AssertEqual(KeyboardKeyToNkKey(KEY_RIGHT), (nk_rune)NK_KEY_RIGHT);
+        AssertEqual(KeyboardKeyToNkKey(KEY_F1), (nk_rune)NK_KEY_F1);
+        AssertEqual(KeyboardKeyToNkKey(KEY_F6), (nk_rune)NK_KEY_F6);
+        AssertEqual(KeyboardKeyToNkKey(KEY_F12), (nk_rune)NK_KEY_F12);
+        AssertEqual(KeyboardKeyToNkKey(KEY_A), (nk_rune)KEY_A);
+        AssertEqual(KeyboardKeyToNkKey(KEY_Z), (nk_rune)KEY_Z);
+    }
+
     CloseWindow();
     TraceLog(LOG_INFO, "================================");
     TraceLog(LOG_INFO, "raylib-nuklear tests succesful");
