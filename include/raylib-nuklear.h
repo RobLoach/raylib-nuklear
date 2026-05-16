cce79b10be48e32e753b854dbb56942c38cb13aa
Rob Loach <robloach@gmail.com>
2026-05-15 23:12:10 -0400

    Update

--- include/raylib-nuklear.h
+++ include/raylib-nuklear.h
@@ -158,6 +158,15 @@ extern "C" {
 #define RAYLIB_NUKLEAR_DEFAULT_ARC_SEGMENTS 20
 #endif  // RAYLIB_NUKLEAR_DEFAULT_ARC_SEGMENTS
 
+#ifndef RAYLIB_NUKLEAR_DOUBLE_CLICK_THRESHOLD
+/**
+ * The amount of time required to wait to determine a mouse click as a double click.
+ *
+ * @see nk_raylib_input_mouse()
+ */
+#define RAYLIB_NUKLEAR_DOUBLE_CLICK_THRESHOLD 0.3
+#endif  // RAYLIB_NUKLEAR_DOUBLE_CLICK_THRESHOLD
+
 /**
  * The user data that's leverages internally through Nuklear.
  */
@@ -950,14 +959,13 @@ nk_raylib_input_mouse(struct nk_context * ctx)
     nk_input_button(ctx, NK_BUTTON_X1, mouseX, mouseY, IsMouseButtonDown(MOUSE_BUTTON_SIDE));
     nk_input_button(ctx, NK_BUTTON_X2, mouseX, mouseY, IsMouseButtonDown(MOUSE_BUTTON_EXTRA));
 
-    // Double-click: track time between left presses; signal NK_BUTTON_DOUBLE within threshold.
+    // Double Click
     {
         static double lastLeftPressTime = 0.0;
         static nk_bool doubleClicking = nk_false;
-        const double doubleClickThreshold = 0.3;
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             double now = GetTime();
-            doubleClicking = (now - lastLeftPressTime <= doubleClickThreshold) ? nk_true : nk_false;
+            doubleClicking = (now - lastLeftPressTime <= RAYLIB_NUKLEAR_DOUBLE_CLICK_THRESHOLD) ? nk_true : nk_false;
             lastLeftPressTime = now;
         }
         else if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
